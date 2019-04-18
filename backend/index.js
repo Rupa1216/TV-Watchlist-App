02f7e68f8077c3b79da7141a5ff5c81d6eac42fb
Srirupa <srirupadatta@pursuit.org>
2019-04-17 21:00:55 -0400

    imported userRouter and implemented corresponding middleware

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 
+const userRouter = require('./routes/user');
+
+
 // MIDDLEWARE NEEDED
 app.use(cors());
 // parse application/x-www-form-urlencoded
@@ -10,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// ROUTES
+app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
     res.json({'test': true})

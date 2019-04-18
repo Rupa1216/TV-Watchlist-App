49c9ee915b3f2c060fd1d060c4849bd3148b5a7c
Srirupa <srirupadatta@pursuit.org>
2019-04-17 20:17:50 -0400

    initial setup of middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
+// MIDDLEWARE NEEDED
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 
 app.listen(3010, () => {

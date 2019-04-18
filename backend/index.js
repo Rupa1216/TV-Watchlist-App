3876f17a1c441d80164fb4ed7b3373688512bb1f
Srirupa <srirupadatta@pursuit.org>
2019-04-17 20:19:26 -0400

    implemented test route and error400 route

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+app.get('/', (req, res) => {
+    res.json({'test': true})
+})
+
+app.use((err, req, res, next) => {
+    res.status(400).json({error: err.toString()});
+});
+
 app.listen(3010, () => {
     console.log('listening on port 3010')
 })

91994ee84511499351e22974c9e655b66c491961
Srirupa <srirupadatta@pursuit.org>
2019-04-17 21:05:23 -0400

    created route to read a user by id

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,18 @@ const express = require('express');
 const userRouter = express.Router();
 const UserService = require('../services/user');
 
+// GET - READ BY USER ID
+userRouter.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    UserService.read(id)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            next(err);
+        })
+});
 
 
 module.exports = userRouter;

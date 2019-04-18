b4a88d860859bbbd92cdb481e06f8f4c51872716
Srirupa <srirupadatta@pursuit.org>
2019-04-17 20:35:23 -0400

    created function to getUser by id

--- backend/services/user.js
+++ backend/services/user.js
@@ -1,3 +1,12 @@
 const { db } = require('../db');
 const UserService = {};
 
+UserService.getUser = (id) => {
+    const sql = `
+    SELECT * 
+    FROM users u
+    WHERE u.id = $[id]
+    `
+    return db.one(sql, { id });
+}
+

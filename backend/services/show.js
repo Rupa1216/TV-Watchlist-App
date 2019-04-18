7b4fa4afef4682b77c48dbf7c351fd1ae1b8cb3a
Srirupa <srirupadatta@pursuit.org>
2019-04-17 21:51:34 -0400

    created function to read all shows

--- backend/services/show.js
+++ backend/services/show.js
@@ -1,5 +1,18 @@
 const { db } = require('../db');
 const ShowService = {};
 
+// GET all shows
+ShowService.getAllShows = () => {
+    const sql = `
+    SELECT * 
+    FROM shows
+    `
+    return db.any(sql);
+}
+
+// GET all shows for specific genre_id
+// GET all shows for specific user_id
+// GET one show
+// POST new show
 
 module.exports = ShowService;

f266eba9da848462c72b89a047f9427873a4e385
Srirupa <srirupadatta@pursuit.org>
2019-04-20 12:15:21 -0400

    inserted Home component into jsx as a Route

--- frontend/tvwatchlist/src/App.js
+++ frontend/tvwatchlist/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './components/navbar';
+import Home from './components/home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <>
+        <HashRouter>
+          <Route path='/' component={Navbar} />
+          <div className="page">
+            <Switch>
+              <Route path='/' exact component={Home} />
+            </Switch>
+          </div>
+        </HashRouter>
+      </>
     );
   }
 }
+
 
 export default App;

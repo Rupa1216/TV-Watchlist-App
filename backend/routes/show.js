e7b31d18c4f37872be55d4c17520f0d11d55a1b7
Srirupa <srirupadatta@pursuit.org>
2019-04-20 10:41:42 -0400

    added route to get all shows

--- backend/routes/show.js
+++ backend/routes/show.js
@@ -2,6 +2,16 @@ const express = require('express');
 const showRouter = express.Router();
 const ShowService = require('../services/show');
 
+// GET all shows
+showRouter.get('/all', (req, res, next) => {
 
+    ShowService.getAllShows()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            next(err);
+        })
+});
 
 module.exports = showRouter;

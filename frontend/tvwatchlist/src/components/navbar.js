66ff5ec2ced3fb654335d911aa8a1b3f171ea394
Srirupa <srirupadatta@pursuit.org>
2019-04-20 12:04:36 -0400

    changed anchor tags to Link tags and removed some bootstrap styling

--- frontend/tvwatchlist/src/components/navbar.js
+++ frontend/tvwatchlist/src/components/navbar.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default (props) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand mb-0 h1" href="/">TV Show Watchlist</a>
+            <Link className="navbar-brand mb-0 h1" to="/">TV Show Watchlist</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/users">Users</a>
+                        <Link className="nav-link" to="/users">Users</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/shows">TV Shows</a>
+                        <Link className="nav-link" to="/shows">TV Shows</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" href="/create" tabIndex="-1" aria-disabled="true">Create Post</a>
+                        <Link className="nav-link" to="/create" tabIndex="-1">Create Post</Link>
                     </li>
                 </ul>
             </div>

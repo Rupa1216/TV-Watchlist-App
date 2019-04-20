25ccce9f73b20fed37a0ae8a921589cb77c28828
Srirupa <srirupadatta@pursuit.org>
2019-04-20 17:04:39 -0400

    initial setup of UserProfile Component

---
+++ frontend/tvwatchlist/src/containers/userprofile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class UserProfile extends React.Component {
+    state = {
+        shows: []
+    }
+
+    render() {
+        return (
+            <h1>User Profile</h1>
+        )
+    }
+}

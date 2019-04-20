a270785d840c06e3e7e7dba6ba3a4a7cea2fa827
Srirupa <srirupadatta@pursuit.org>
2019-04-20 12:57:04 -0400

    updated inline style property name

--- frontend/tvwatchlist/src/components/home.js
+++ frontend/tvwatchlist/src/components/home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default (props) => {
     return (
-        <div class="card bg-light mt-3 mx-auto" style={{"max-width": "21rem"}}>
-            <div class="card-header">Welcome to the TV Show Watchlist App!</div>
+        <div className="card bg-light mt-3 mx-auto" style={{"maxWidth": "21rem"}}>
+            <div className="card-header">Welcome to the TV Show Watchlist App!</div>
         </div>
     )
 }

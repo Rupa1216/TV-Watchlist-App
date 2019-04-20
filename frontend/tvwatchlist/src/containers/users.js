34719bd9eb27c46643fc5890d3732cf05f7c2972
Srirupa <srirupadatta@pursuit.org>
2019-04-20 15:56:07 -0400

    implemented componentDidMount; rendered a list of all users

--- frontend/tvwatchlist/src/containers/users.js
+++ frontend/tvwatchlist/src/containers/users.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import axios from 'axios';
+import User from '../components/user';
 
 export default class Users extends React.Component {
 
     state = {
-        username: "Heri00",
+        username: "PamAbr",
         userID: 20,
         users: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3010/users/')
+            .then((res) => {
+                console.log(res.data);
+                res.data.map((e, i) => {
+                    this.setState({
+                        users: [...this.state.users,
+                        { id: e.id, username: e.username }]
+                    })
+                })
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 
     render() {
@@ -13,16 +31,20 @@ export default class Users extends React.Component {
 
         return (
             <>
-            <div className='ml-3 mt-4 mb-5'>
-                <h2>Welcome to TV Show Watchlist, {username}!</h2>
-                <h4>Your user ID is: {userID}.</h4>
-            </div>
-            <div className='ml-3'>
-                <h4>Master List of All Users:</h4>
-                <ul>
-
-                </ul>
-            </div>
+                <div className='ml-3 mt-4 mb-5'>
+                    <h2>Welcome to TV Show Watchlist, {username}!</h2>
+                    <h4>Your user ID is: {userID}.</h4>
+                </div>
+                <div className='ml-3'>
+                    <h4>Master List of All Users:</h4>
+                    <ul>
+                        {users.map((e, i) => {
+                            return <li key={i}>
+                                <User data={e} />
+                            </li>
+                        })}
+                    </ul>
+                </div>
             </>
         )
     }

fc3092f37a8fe839e37842d280bbffd1bba5258d
Srirupa <srirupadatta@pursuit.org>
2019-04-20 11:01:22 -0400

    added routes to get show(s) by genre ID, by user ID, and by show ID

--- backend/routes/show.js
+++ backend/routes/show.js
@@ -14,4 +14,43 @@ showRouter.get('/all', (req, res, next) => {
         })
 });
 
+// GET all shows for specific genre_id 
+showRouter.get('/bygenre/:genre_id', (req, res, next) => {
+    const { genre_id } = req.params;
+
+    ShowService.getByGenre(genre_id)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            next(err);
+        })
+});
+
+// GET all shows for specific user_id 
+showRouter.get('/byuser/:user_id', (req, res, next) => {
+    const { user_id } = req.params;
+
+    ShowService.getByUser(user_id)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            next(err);
+        })
+});
+
+// GET one show 
+showRouter.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    ShowService.read(id)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            next(err);
+        })
+});
+
 module.exports = showRouter;

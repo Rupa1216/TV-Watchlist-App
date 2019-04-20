61ca8f1bb89ddc3455f3deb2be52fb4db4403b04
Srirupa <srirupadatta@pursuit.org>
2019-04-20 11:38:49 -0400

    initial navbar setup

---
+++ frontend/tvwatchlist/src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <span class="navbar-brand mb-0 h1">TV Show Watchlist</span>
+        </nav>
+    )
+}

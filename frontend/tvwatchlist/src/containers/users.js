fe7b2da7084f01fe6a90fe317c5e4e4ec64f6df8
Srirupa <srirupadatta@pursuit.org>
2019-04-20 12:47:07 -0400

    added state; set up dynamic rendering of logged in user and all users

--- frontend/tvwatchlist/src/containers/users.js
+++ frontend/tvwatchlist/src/containers/users.js
@@ -2,11 +2,28 @@ import React from 'react';
 
 export default class Users extends React.Component {
 
+    state = {
+        username: "Heri00",
+        userID: 20,
+        users: []
+    }
+
     render() {
+        const { username, userID, users } = this.state;
+
         return (
-            <div className='ml-3 mt-4'>
-                <h1>Welcome!</h1>
+            <>
+            <div className='ml-3 mt-4 mb-5'>
+                <h2>Welcome to TV Show Watchlist, {username}!</h2>
+                <h4>Your user ID is: {userID}.</h4>
             </div>
+            <div className='ml-3'>
+                <h4>Master List of All Users:</h4>
+                <ul>
+
+                </ul>
+            </div>
+            </>
         )
     }
 }

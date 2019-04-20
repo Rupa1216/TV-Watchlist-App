db1e133927f8d6008d7a75aa97ceaeb4fd09f278
Srirupa <srirupadatta@pursuit.org>
2019-04-20 18:39:07 -0400

    implemented componentDidMount; rendered list of shows according to user ID

--- frontend/tvwatchlist/src/containers/userprofile.js
+++ frontend/tvwatchlist/src/containers/userprofile.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import axios from 'axios';
+import Show from '../components/show';
 
 export default class UserProfile extends React.Component {
     state = {
         shows: []
     }
 
+    componentDidMount() {
+        const { id } = this.props.match.params;
+
+        axios.get('http://localhost:3010/shows/byuser/'+id)
+        .then((res) => {
+            console.log(res.data);
+            res.data.map((e, i) => {
+                this.setState({
+                    shows: [...this.state.shows,
+                    { id: e.id, img_url: e.img_url, title: e.title, }]
+                })
+            })
+        })
+    }
+
     render() {
+        console.log(this.state)
+        const { shows } = this.state;
+
         return (
-            <h1>User Profile</h1>
+            <div className='container row mx-auto'>
+                {
+                    shows.map((e, i) => {
+                        return <Show data={e} key={i} />
+                    })
+                }
+            </div>
         )
     }
 }

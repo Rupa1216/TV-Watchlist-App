d2a51c14ffbdb36c112352c80919835dd4c2afa2
Srirupa <srirupadatta@pursuit.org>
2019-04-20 12:18:05 -0400

    created display message for Home Component

---
+++ frontend/tvwatchlist/src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div class="card bg-light mt-3 mx-auto" style={{"max-width": "21rem"}}>
+            <div class="card-header">Welcome to the TV Show Watchlist App!</div>
+        </div>
+    )
+}
+

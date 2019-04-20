2c31a887d5cc81a580957e7e99621d2867b361f8
Srirupa <srirupadatta@pursuit.org>
2019-04-20 12:35:21 -0400

    initial setup of Users page

---
+++ frontend/tvwatchlist/src/containers/users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Users extends React.Component {
+
+    render() {
+        return (
+            <div className='ml-3 mt-4'>
+                <h1>Welcome!</h1>
+            </div>
+        )
+    }
+}

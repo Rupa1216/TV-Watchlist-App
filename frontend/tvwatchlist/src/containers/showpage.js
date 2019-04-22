5f96a84ebdd3a3e8ea0444a28107690015980afc
Srirupa <srirupadatta@pursuit.org>
2019-04-21 22:53:56 -0400

    initial setup of individual Show page

---
+++ frontend/tvwatchlist/src/containers/showpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+import Show from '../components/show';
+
+export default class ShowPage extends React.Component {
+
+    render() {
+        return (
+            <h1>Show Page</h1>
+        )
+    }
+}

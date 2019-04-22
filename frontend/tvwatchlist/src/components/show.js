e265a790336a68307b7c6a1e6d7ccbbe6b961474
Srirupa <srirupadatta@pursuit.org>
2019-04-21 22:40:57 -0400

    added the genre of each show; restructured divs

--- frontend/tvwatchlist/src/components/show.js
+++ frontend/tvwatchlist/src/components/show.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default (props) => {
+    const { id } = props.data;
+
     return (
-        <div className='col-6 mt-5 text-center'>
-            <Link to='/show'>
-                <img src={props.data.img_url} alt={props.data.title} />
-                <h5>{props.data.title}</h5>
-                {/* <h6>{props.genre_name}</h6> */}
-            </Link>
+        <div className='d-flex col-6 mt-5'>
+            <div>
+                <Link to={'/show/'+id}>
+                    <img src={props.data.img_url} alt={props.data.title} />
+                </Link>
+            </div>
+            <div className='my-auto ml-3'>
+                <Link to={'/show/'+id}>
+                    <h5>{props.data.title}</h5>
+                </Link>
+                {/* <h6>Genre</h6> */}
+                <h6>{props.data.genre_name}</h6>
+            </div>
         </div>
     )
 }

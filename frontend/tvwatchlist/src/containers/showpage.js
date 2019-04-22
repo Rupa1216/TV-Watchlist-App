c3cd37cf21d0a2c71fa446446fb7454a32a8e9cf
Srirupa <srirupadatta@pursuit.org>
2019-04-22 19:38:59 -0400

    added componentDidMount method; rendered show info on page

--- frontend/tvwatchlist/src/containers/showpage.js
+++ frontend/tvwatchlist/src/containers/showpage.js
@@ -8,10 +8,29 @@ export default class ShowPage extends React.Component {
         comments: []
     }
 
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        axios.get('http://localhost:3010/shows/' + id)
+            .then((res) => {
+                this.setState({show: res.data})
+            })
+    }
+
     render() {
+        const {show} = this.state;
         return (
             <>
-            <h1>Show Page</h1>
+                <h1>Show Page</h1>
+                <div className='mt-5'>
+                <div>
+                    <img src={show.img_url} alt={show.title} />
+                </div>
+                <div className='my-auto ml-3'>
+                    <h5>{show.title}</h5>
+                    <h6>Genre</h6>
+                    {/* <h6>{show.genre_name}</h6> */}
+                </div>
+            </div>
             </>
         )
     }

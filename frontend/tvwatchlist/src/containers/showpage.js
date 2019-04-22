035858d3af2cb722459e62405350d8cb0a044759
Srirupa <srirupadatta@pursuit.org>
2019-04-22 01:59:18 -0400

    added initial state

--- frontend/tvwatchlist/src/containers/showpage.js
+++ frontend/tvwatchlist/src/containers/showpage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import axios from 'axios';
-import Show from '../components/show';
 
 export default class ShowPage extends React.Component {
+    state = {
+        show: {},
+        users: [],
+        comments: []
+    }
 
     render() {
         return (
+            <>
             <h1>Show Page</h1>
+            </>
         )
     }
 }

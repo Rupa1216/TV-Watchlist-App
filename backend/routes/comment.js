23caf1b4db572e0cdd19b6cb1aa1273bdec14fbc
Srirupa <srirupadatta@pursuit.org>
2019-04-23 19:11:15 -0400

    inital setup of commentRouter

---
+++ backend/routes/comment.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const commentRouter = express.Router();
+const CommentService = require('../services/comment');
+
+// GET all comments for specific show_id
+
+
+// POST new comment
+
+
+module.exports = commentRouter;

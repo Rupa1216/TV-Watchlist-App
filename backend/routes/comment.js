a04d4c55ecb80eb15b791b935cfe1f38fb3bc000
Srirupa <srirupadatta@pursuit.org>
2019-04-23 19:20:50 -0400

    added route to GET all comments for a specific show ID

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -3,7 +3,17 @@ const commentRouter = express.Router();
 const CommentService = require('../services/comment');
 
 // GET all comments for specific show_id
+commentRouter.get('/:show_id', (req, res, next) => {
+    const { show_id } = req.params;
 
+    CommentService.read(show_id)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            next(err);
+        })
+});
 
 // POST new comment
 

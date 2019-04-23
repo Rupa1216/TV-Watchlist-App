1107961615f98f238d6385bdda09e232d61b0241
Srirupa <srirupadatta@pursuit.org>
2019-04-23 16:54:52 -0400

    added service to GET comment by show ID

--- backend/services/comment.js
+++ backend/services/comment.js
@@ -2,5 +2,19 @@ const { db } = require('../db');
 const CommentService = {};
 
 
+// GET all comments for specific show_id
+CommentService.read = (id) => {
+    const sql = `
+    SELECT c.id, c.comment_body, c.user_id, s.title, s.img_url
+    FROM comments c
+    LEFT JOIN shows s
+    ON c.show_id = s.id 
+    WHERE
+    c.show_id = $[id]
+    `
+    return db.any(sql, { id });
+}
+
+// POST new comment
 
 module.exports = CommentService;

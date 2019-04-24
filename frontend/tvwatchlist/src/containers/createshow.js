4fd0783c0a9d9f6ba974fc721aa140680c687ec8
Srirupa <srirupadatta@pursuit.org>
2019-04-24 14:11:20 -0400

    rendered list of genres from db in dropdown

--- frontend/tvwatchlist/src/containers/createshow.js
+++ frontend/tvwatchlist/src/containers/createshow.js
@@ -26,8 +26,11 @@ export default class CreateShow extends React.Component {
     }
 
     render() {
-        const { user_id } = this.state;
+        const { user_id, genres } = this.state;
         console.log('create show state', this.state);
+        const genreList = genres.map((e, i) => {
+            return <option key={i} value={i}>{e}</option>
+        })
 
         return (
             <>
@@ -45,9 +48,7 @@ export default class CreateShow extends React.Component {
                         </div>
                         <select className="custom-select" id="inputGroupSelect01">
                             <option defaultValue>Choose...</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                            {genreList}
                         </select>
                     </div>
                     <button type="submit" className="btn btn-outline-secondary col-4 ml-3">Post new show for User ID: {user_id}</button>

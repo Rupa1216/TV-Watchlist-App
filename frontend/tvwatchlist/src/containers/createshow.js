10dbd2bcc9d110aa154c037b3d6387a367fc89ce
Srirupa <srirupadatta@pursuit.org>
2019-04-24 04:55:17 -0400

    rendered initial Create Show form

--- frontend/tvwatchlist/src/containers/createshow.js
+++ frontend/tvwatchlist/src/containers/createshow.js
@@ -10,8 +10,32 @@ export default class CreateShow extends React.Component {
     }
 
     render() {
+        const {user_id} = this.state;
+
         return (
-            <h1 className='ml-3 mt-3 mb-4'>Add new show to watch...</h1>
+            <>
+                <h1 className='ml-5 pl-3 mt-4 mb-5'>Add new show to watch...</h1>
+                <form className='ml-5'>
+                    <div className="form-group col-4 mb-3">
+                        <input className='form-control mb-2' type='text' placeholder='Insert Title...' />
+                    </div>
+                    <div className="form-group col-4">
+                        <input className='form-control mb-2' type='text' placeholder='Insert Image URL...' />
+                    </div>
+                    <div className="input-group col-4 mb-5">
+                        <div className="input-group-prepend">
+                            <label className="input-group-text" htmlFor="inputGroupSelect01">Genre</label>
+                        </div>
+                        <select className="custom-select" id="inputGroupSelect01">
+                            <option defaultValue>Choose...</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="btn btn-outline-secondary col-4 ml-3">Post new show for User ID: {user_id}</button>
+                </form>
+            </>
         )
     }
 }

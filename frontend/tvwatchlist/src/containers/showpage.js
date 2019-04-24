04a0e7fdf582426906b122d2d02dfc2a9e4adea2
Srirupa <srirupadatta@pursuit.org>
2019-04-23 22:21:04 -0400

    rendered username watching current show_id; imported Comments component and rendered list of comments by username

--- frontend/tvwatchlist/src/containers/showpage.js
+++ frontend/tvwatchlist/src/containers/showpage.js
@@ -1,48 +1,44 @@
 import React from 'react';
 import axios from 'axios';
+import Comments from './comments';
 
 export default class ShowPage extends React.Component {
     state = {
         show: {},
-        title: '',
-        users: [],
-        comments: []
     }
-
-    // getShowInfo = () => {
-    //     const { id } = this.props.match.params;
-    //     return axios.get('http://localhost:3010/shows/' + id)
-    // }
-
-    // getWhosWatching = () => {
-    //     const { title } = this.state;
-    //     const titleInURL = title.split(' ').join('')
-    //     return axios.get('http://localhost:3010/shows/' + titleInURL)
-    // }
 
     componentDidMount() {
         const { id } = this.props.match.params;
-        
+
         axios.get('http://localhost:3010/shows/' + id)
             .then((res) => {
-                console.log('data', res.data)
-                this.setState({ show: res.data })
+                this.setState({ show: res.data, title: res.data.title })
+            })
+            .catch((err) => {
+                console.log(err);
             })
     }
 
     render() {
-        const { show } = this.state;
+        const { show, comments } = this.state;
+        console.log('this is the state', this.state);
+
         return (
             <>
                 <div className='d-flex mt-5 ml-5'>
                     <div>
                         <img src={show.img_url} alt={show.title} style={{ width: '200px', height: '300px' }} />
                     </div>
-                    <div className='my-auto ml-3'>
+                    <div className='mt-3 ml-3'>
                         <h3>{show.title}</h3>
                         <h5>{show.genre_name}</h5>
                     </div>
                 </div>
+                <div className='mt-3 ml-5'>
+                    <h5>Being watched by {show.username}</h5>
+                </div>
+                <hr />
+                <Comments />
             </>
         )
     }

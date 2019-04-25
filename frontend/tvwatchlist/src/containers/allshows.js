025dc1362224175ba5e62e68ec4617740c32c2c4
Srirupa <srirupadatta@pursuit.org>
2019-04-24 20:01:49 -0400

    initial setup of Shows page

---
+++ frontend/tvwatchlist/src/containers/allshows.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import axios from 'axios';
+
+
+export default class AllShows extends React.Component {
+    render() {
+        return (
+            <h2 className='mt-3 ml-3'>Master List of TV Shows</h2>
+        )
+    }
+}

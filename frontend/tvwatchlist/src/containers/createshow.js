d5332e4cac3f3e68bb186151801676e9bc7a4b31
Srirupa <srirupadatta@pursuit.org>
2019-04-24 20:03:07 -0400

    implemented API call to post a new show; rendered alert message on success

--- frontend/tvwatchlist/src/containers/createshow.js
+++ frontend/tvwatchlist/src/containers/createshow.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class CreateShow extends React.Component {
     state = {
@@ -7,7 +8,9 @@ export default class CreateShow extends React.Component {
         img_url: '',
         genres: [],
         selectedGenre: '',
-        user_id: 5
+        selectedGenreID: '',
+        user_id: 5,
+        show_id: ''
     }
 
     handleChange = (e) => {
@@ -15,32 +18,61 @@ export default class CreateShow extends React.Component {
     }
 
     handleSelectedGenre = (e) => {
-        this.setState({ selectedGenre: e.target.value })
+        this.setState({ selectedGenre: e.target.name, selectedGenreID: e.target.value })
+        // genres.forEach((e) => {
+        //     if (selectedGenre === e.genre_name) {
+        //         this.setState({ selectedGenreID: e.id })
+        //     }
+        // })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const { title, img_url, user_id, selectedGenre, selectedGenreID } = this.state;
+        
+        axios.post('http://localhost:3010/shows/', {
+            title: title,
+            img_url: img_url,
+            user_id: user_id,
+            genre_id: selectedGenreID
+        })
+            .then((res) => {
+                const { id } = res.data;
+                this.setState({ show_id: id })
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
     componentDidMount() {
-        const { genres } = this.state;
 
         axios.get('http://localhost:3010/genres/')
             .then((res) => {
-                res.data.forEach((e) => {
-                    genres.push(e.genre_name)
-                })
-                this.setState({ genres })
+                // res.data.forEach((e) => {
+                //     genres.push(e.genre_name)
+                // })
+                // this.setState({ genres })
+                this.setState({ genres: res.data })
             })
     }
 
     render() {
-        const { user_id, genres } = this.state;
+        const { user_id, genres,  show_id } = this.state;
         console.log('create show state', this.state);
         const genreList = genres.map((e, i) => {
-            return <option key={i} value={e.value}>{e}</option>
+            return <option key={i} value={e.id} name={e.genre_name}>{e.id} - {e.genre_name}</option>
         })
+        const alert = <div className="alert alert-success" role="alert">
+        Success! You've added a new show. Click <Link to={'/show/'+show_id} className="alert-link">here</Link> to view.
+    </div>
 
         return (
             <>
                 <h1 className='ml-5 pl-3 mt-4 mb-5'>Add new show to watch...</h1>
-                <form className='ml-5'>
+                { show_id ? alert : null }
+                <form className='ml-5' onSubmit={this.handleSubmit}>
                     <div className="form-group col-4 mb-3">
                         <input className='form-control mb-2' onChange={this.handleChange} name='title' type='text' placeholder='Insert Title...' />
                     </div>
@@ -51,7 +83,7 @@ export default class CreateShow extends React.Component {
                         <div className="input-group-prepend">
                             <label className="input-group-text" htmlFor="inputGroupSelect01">Genre</label>
                         </div>
-                        <select onChange={this.handleSelectedGenre} value={this.state.selectedGenre} className="custom-select" id="inputGroupSelect01">
+                        <select onChange={this.handleSelectedGenre} value={this.state.selectedGenre} className="custom-select">
                             <option defaultValue>Choose...</option>
                             {genreList}
                         </select>

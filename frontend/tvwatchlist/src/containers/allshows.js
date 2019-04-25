2f9e5d420e8fe5cb90086ecacf151b93549b4043
Srirupa <srirupadatta@pursuit.org>
2019-04-24 20:21:38 -0400

    implemented API call to get all shows' data on page load

--- frontend/tvwatchlist/src/containers/allshows.js
+++ frontend/tvwatchlist/src/containers/allshows.js
@@ -3,7 +3,21 @@ import axios from 'axios';
 
 
 export default class AllShows extends React.Component {
+    state = {
+        shows: []
+    }
+    
+    componentDidMount() {
+        axios.get('http://localhost:3010/shows/all/')
+            .then((res) => {
+                console.log(res.data, 'res.data all shows')
+                this.setState({ shows: res.data })
+            })
+    }
+    
     render() {
+        console.log('all shows state', this.state);
+
         return (
             <h2 className='mt-3 ml-3'>Master List of TV Shows</h2>
         )
